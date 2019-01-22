03e74a653a3f2ba414630acd22714f03373976ee
Daniel Pietrucha <d.pietrucha@gmi.pl>
2019-01-22 20:39:59 +0100

    Move hero-details to separate component

--- HeroesApp/src/app/components/components.module.ts
+++ HeroesApp/src/app/components/components.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     CommonModule,
